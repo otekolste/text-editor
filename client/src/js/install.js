69891107215fc286d284fd9f4fe278d4887c10e3
oliviatek <oliviatekolste@gmail.com>
2024-10-06 18:38:48 -0400

    filled in beforeinstallprompt, click, and appinstalled commands

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,34 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
+
+// Tutorial referenced: https://web.dev/articles/codelab-make-installable
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  console.log("👍", "beforeinstallprompt", event);
+  window.deferredPrompt = event;
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    console.log("a");
+    // The deferred prompt isn't available.
+    return;
+  }
+  promptEvent.prompt();
+  // Log the result
+  const result = await promptEvent.userChoice;
+  console.log("👍", "userChoice", result);
+  // Reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log("👍", "appinstalled", event);
+  window.deferredPrompt = null;
+});
